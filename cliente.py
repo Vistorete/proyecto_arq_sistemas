090e8900037e37daf1edfe69c84c7a7cb73c436a
Victor <victor.sanchez1@mail.udp.cl>
2021-07-04 18:32:01 -0400

    update

--- cliente.py
+++ cliente.py
@@ -1,1 +1,31 @@
 # test
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 5000)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+try:
+
+    # Send data
+    message = b'00015sumar 1 2'
+    print('sending {!r}'.format(message))
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while True:
+        data = sock.recv(4096)
+        amount_received += len(data)
+        print('received {!r}'.format(data))
+
+finally:
+    print('closing socket')
+    sock.close()
---
+++ servicio_test.py
@@ -0,0 +1,55 @@
+# test de servicio
+# "sinit" crea el servicio
+# transaccion: 
+import socket
+import sys
+
+def enviarTransaccion(sock, servicio, contenido):
+    # Generacion de la transaccion
+    # validacion de argumentos
+    if len(servicio) < 5 or len(contenido) < 1:
+        print("los argumentos no cumplen con los requerimietos")
+        return
+    # contruccion de la transaccion
+    largoTransaccion = str(len(contenido) + 10)
+    while len(largoTransaccion) <5:
+        largoTransaccion = "0" + largoTransaccion
+
+    transaccion = largoTransaccion + servicio + contenido
+    print("transaccion:",transaccion)
+    sock.sendall(transaccion.encode())
+
+
+    # Respuesta de la transaccion
+    amount_received = 0
+    while True:
+        data = sock.recv(4096)
+        amount_received += len(data)
+        print('received {!r}'.format(data))
+
+def registrarServicio(socket, nombreServicio="test1"):
+    transaccion = "00010sinit" + nombreServicio
+    
+    
+
+
+if __name__ == "__main__":
+    # Conexion con el bus
+    try:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = ('localhost', 5000)
+        print('conectandose a {} puerto {}'.format(*server_address))
+        sock.connect(server_address)
+    # En caso de error cierra la aplicacion
+    except: 
+        print("no se pudo conectar con el bus")
+        quit() 
+
+    
+
+    enviarTransaccion(sock, "sinit","test1")
+    print('cerrando socket')
+    sock.close()

f400ad4a9075a5cd25bf153e4c59145e515a4b6a
Victor <victor.sanchez1@mail.udp.cl>
2021-07-10 01:37:37 -0400

    Create servicio_buscarLocal.py

---
+++ servicios/servicio_buscarLocal.py
@@ -0,0 +1,31 @@
+from os import curdir
+import socket
+import socket, sys, json
+from gestor_base import conexion, crearBase
+from funcionesGenerales import enviarTransaccion, escucharBus, registrarServicio
+
+SERVICIO = "busc9" # Buscar
+
+if __name__ == "__main__":
+    try:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = ('localhost', 5000)
+        print('Servicio: Conectandose a {} puerto {}'.format(*server_address))
+        sock.connect(server_address)
+    # En caso de error cierra la aplicacion
+    except: 
+        print("no se pudo conectar con el bus")
+        quit() 
+
+    registrarServicio(sock, SERVICIO)
+    while True:
+        serv, msg=escucharBus(sock)
+        if serv != SERVICIO:
+            respuesta = {"respuesta":"servicio equivocado"}
+            enviarTransaccion(sock,json.dumps(respuesta), SERVICIO)
+        else:
+            # Lo que debe hacer el servicio
+            print(msg)

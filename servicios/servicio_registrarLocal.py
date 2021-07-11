26478c0b4dcc33c4be1e778251f56dfcafce5024
Victor <victor.sanchez1@mail.udp.cl>
2021-07-10 22:18:11 -0400

    Create servicio_registrarLocal.py

---
+++ servicios/servicio_registrarLocal.py
@@ -0,0 +1,30 @@
+# test de servicio
+# "sinit" crea el servicio
+# transaccion: 
+from os import curdir
+import socket
+import socket, sys, json
+from gestor_base import conexion, crearBase
+from funcionesGenerales import enviarTransaccion, escucharBus, registrarServicio
+
+SERVICIO = "rglc9" #Registro de usuarios
+
+if __name__ == "__main__":
+    # Conexion con el bus
+    try:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = ('localhost', 5000)
+        print('Servicio: Conectandose a {} puerto {}'.format(*server_address))
+        sock.connect(server_address)
+    # En caso de error cierra la aplicacion
+    except: 
+        print("no se pudo conectar con el bus")
+        quit() 
+
+    registrarServicio(sock, SERVICIO)
+    while True:
+        serv, msg=escucharBus(sock)
+        print(serv, msg)

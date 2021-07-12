dacc3f10e3f60ede49a3add695b59f2ddf8494c8
CristobalFlores <52360895+CristobalFlores@users.noreply.github.com>
2021-07-11 20:58:14 -0400

    Update clienteUsuario.py

--- cliente/clienteUsuario.py
+++ cliente/clienteUsuario.py
@@ -213,7 +213,7 @@ def menuBuscarLocal():
                 enviarTransaccion(sock,json.dumps(contenido),REALIZAR_RESERVAS)
                 serv2, mensaje2=escucharBus(sock)
                 print(serv2,mensaje2)
-                if mensaje2["respuesta"] != "Error":
+                if mensaje2["respuesta"] != "error":
                     menu = f"""
         ╔═══════════════════════════════════════════════════════════════════════╗
         ║ Rezerva realisada con exito                                           ║

06464b723225d8cffa48d3a9d3497cb15510cc32
Victor <victor.sanchez1@mail.udp.cl>
2021-07-13 16:50:34 -0400

    Update servicio_registrarLocal.py

--- servicios/servicio_registrarLocal.py
+++ servicios/servicio_registrarLocal.py
@@ -28,34 +28,7 @@ if __name__ == "__main__":
 
     registrarServicio(sock, SERVICIO)
     while True:
-<<<<<<< Updated upstream
-        # try:
-        serv, msg=escucharBus(sock) 
-        print(serv, msg) #{{"id_administrador": 8, "nombre": "Los Pollos Hermanos", "descripcion": "Basado en BrBa", "comuna": "Condado de colorado", "tipo_comida": "china peruana", "reservas_maxima": "20"}}
-            # Valida al usuario
-        msg = json.loads(msg)
-        cursor = conexion.execute("SELECT * FROM usuario WHERE id = ? AND rol = 'administrador'", (msg["id_administrador"],))
-        usuario = cursor.fetchone()
-        print("Servicio: Usuario",usuario)
-        if usuario:
-            # Revisa si ya tiene un local
-            cursor = conexion.execute("SELECT * FROM local WHERE id_administrador = ?", ( msg["id_administrador"],))
-            local = cursor.fetchone()
-            # Si no hay local lo inserta
-            print("Servicio: Local",local)
-            if local == None:
-                cursor = conexion.execute("INSERT INTO local(id_administrador,nombre, descripcion,comuna,tipo_comida,reservas_maxima, horario_inicio,horario_cierre) VALUES(?,?,?,?,?,?,?,?)",(msg["id_administrador"],msg["nombre"],msg["descripcion"], msg["comuna"].lower(), msg["tipo_comida"],int(msg["reservas_maxima"]), msg["h_inicio"],msg["h_termino"]))
-                conexion.commit()
-                respuesta = {"respuesta":"Se registrado correctamente"}
-                enviarTransaccion(sock,json.dumps(respuesta), SERVICIO)
-            # Si hay local lo actualiza
-            else:
-                # Hacer update
-                cursor = conexion.execute("UPDATE local SET nombre = ?, descripcion = ?,comuna = ?,tipo_comida= ?,reservas_maxima= ? WHERE id_administrador =?, horario_inicio = ?, horario_cierre = ? ",(msg["nombre"],msg["descripcion"], msg["comuna"].lower(), msg["tipo_comida"],int(msg["reservas_maxima"]), msg["h_inicio"],msg["h_termino"], msg["id_administrador"]))
-                conexion.commit()
-                respuesta = {"respuesta":"Se ha actualizado correctamente"}
-                enviarTransaccion(sock,json.dumps(respuesta), SERVICIO)
-=======
+
         try:
             serv, msg=escucharBus(sock) 
             print(serv, msg) #{{"id_administrador": 8, "nombre": "Los Pollos Hermanos", "descripcion": "Basado en BrBa", "comuna": "Condado de colorado", "tipo_comida": "china peruana", "reservas_maxima": "20"}}
@@ -88,7 +61,7 @@ if __name__ == "__main__":
             respuesta = {"error":"No se pudo realizar la solicitud."}
             enviarTransaccion(sock, json.dumps(respuesta), SERVICIO)
             print(e)
->>>>>>> Stashed changes
+
 
 
 

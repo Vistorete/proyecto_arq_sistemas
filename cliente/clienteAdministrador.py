aac7eeb5e3d91d0eb04b47538f3103a10cabdc12
CristobalFlores <52360895+CristobalFlores@users.noreply.github.com>
2021-07-13 18:02:07 -0400

    Update clienteAdministrador.py

--- cliente/clienteAdministrador.py
+++ cliente/clienteAdministrador.py
@@ -113,9 +113,9 @@ def menuReservas():
     enviarTransaccion(sock,json.dumps(contenido),REVISAR_RESERVAS)
     serv, mensaje=escucharBus(sock)
     respuesta = json.loads(mensaje[2:])
-    resp = respuesta["reservas"]
     # print(respuesta)
-    if resp != " ":
+    if respuesta != " ":
+        resp = respuesta["reservas"]
         for reserva in resp:
             info = f"""            ════════════════════════════════
                 ID usuario: {reserva[0]}
